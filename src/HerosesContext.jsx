25456b943693e672e14d8799445db32bf1396c21
Tomek Sochacki <zbudujmydompl@gmail.com>
2024-01-09 17:40:21 +0100

    context

--- src/HerosesContext.jsx
+++ src/HerosesContext.jsx
@@ -1,16 +1,35 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const HerosesContext = createContext([]);
 
 export const HerosesContextProvider = ({children}) => {
     const [heroses, setHeroses] = useState([]);
 
+    useEffect(() => {
+        Promise.all([
+            fetchRandomHeroes(),
+            fetchRandomHeroes(),
+            fetchRandomHeroes(),
+          ]).then((data) => setHeroses(data));
+    }, [])
+
     const addHero = (hero) => {
         setHeroses(prevState => [...prevState, hero]);
     }
 
+    const removeHero = (id) => {
+        const updatedHeroses = heroses.filter(hero => hero.id !== id);
+        setHeroses(updatedHeroses);
+    }
+
+    const removeallHeroses = () => {
+        setHeroses([]);
+    }
+
     return <HerosesContext.Provider value={{
         heroses, 
-        addHero
+        addHero,
+        removeHero,
+        removeallHeroses
     }}>{children}</HerosesContext.Provider>
 }
